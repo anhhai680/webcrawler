9fc3c8fd4133ac25e09e0fb423964f18b1cb0d2e
Luis Dinh <anhhai680@gmail.com>
2019-07-02 16:37:48 +0700

    Added a ProductItem model in items.py

--- webcrawler/items.py
+++ webcrawler/items.py
@@ -12,3 +12,12 @@ class WebcrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class ProductItem(scrapy.Item):
+    title = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
+    #specifications = scrapy.Field()
+    link = scrapy.Field()
+    images = scrapy.Field()
+    pass
--- webcrawler/spiders/cellphones.py
+++ webcrawler/spiders/cellphones.py
@@ -2,6 +2,7 @@
 import scrapy
 import logging
 from scrapy.spiders import SitemapSpider
+from ..items import ProductItem
 
 logger = logging.getLogger('cellphones_logger')
 
@@ -20,35 +21,16 @@ class CellphonesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        #logger.info('Parse url: %s',response.url)
-        # def extract_with_css(item, query):
-        #     return item.css(query).get(default='').strip()
-
-        # def extract_price(item):
-        #     price = item.css(
-        #         'p.special-price>span::text').get(default='').strip()
-        #     if price == '':
-        #         price = item.css(
-        #             'span.regular-price>span::text').get(default='').strip()
-        #     return price
-
-        for item in response.css('li.cate-pro-short'):
-            # scraped_info = {
-            #     'title': extract_with_css(item, 'h3::text'),
-            #     'link': item.css('a').xpath('@href').get(),
-            #     'img': item.css('img').xpath('@src').get(),
-            #     'old_price': extract_with_css(item, 'p.old-price>span::text'),
-            #     'price': extract_price(item),
-            #     'gift': extract_with_css(item, 'p.gift-cont::text')
-            # }
-            # yield scraped_info
-            link_product = item.css('a').xpath('@href').get()
+        # logger.info('Parse url: %s',response.url)
+        # Get all product links on current page
+        for link_product in response.css('div.lt-product-group-image>a::attr(href)'):
             if link_product is not None:
-                yield response.follow(link_product,self.parse_product_detail)
+                yield response.follow(link_product, self.parse_product_detail)
 
         links = response.xpath(
             '//ul[@class="pagination"]/li[not(contains(@class,"active"))]/a/@href').getall()
 
+        # Following to scrape for next page
         for next_page in links:
             if next_page is not None:
                 yield response.follow(next_page, callback=self.parse)
@@ -58,7 +40,7 @@ class CellphonesSpider(scrapy.Spider):
     def parse_product_detail(self, response):
         def extract_with_css(query):
             return response.css(query).get(default='').strip()
-        
+
         def extract_with_xpath(query):
             return response.xpath(query).get(default='').strip()
 
@@ -69,16 +51,42 @@ class CellphonesSpider(scrapy.Spider):
                 price = response.css(
                     'span.regular-price>span::text').get(default='').strip()
             return price
-        
-        def extract_product_gallery(query):
-            return response.css(query).xpath("@src").getall()
 
-        product_info = {
-            'title': extract_with_css('h1::text'),
-            'description': extract_with_xpath('//meta[@name="description"]/@content'),
-            'price': extract_price(),
-            'images': extract_product_gallery('div.product-image-gallery>img'),
-            'link': response.url
-        }
-        yield product_info
+        def extract_product_gallery():
+            gallery = response.css(
+                'div.product-image-gallery>img::attr(src)').getall()
+            if len(gallery) <= 0:
+                gallery = response.css(
+                    'div.product-img-box>img::attr(src)').get()
+            return gallery
+
+        # product_info = {
+        #     'title': extract_with_css('h1::text'),
+        #     'description': extract_with_xpath('//meta[@name="description"]/@content'),
+        #     'price': extract_price(),
+        #     'images': extract_product_gallery('div.product-image-gallery>img'),
+        #     'link': response.url
+        # }
+
+        product_link = response.url
+        # Continues scrape other product model's link on this page
+        for other_model_link in response.css('div.linked>div>a::attr(href)'):
+            if other_model_link is not None and other_model_link != product_link:
+                yield response.follow(other_model_link, self.parse_product_detail)
+
+        products = ProductItem()
+
+        product_title = extract_with_css('h1::text')
+        product_desc = extract_with_xpath(
+            '//meta[@name="description"]/@content')
+        product_price = extract_price()
+        product_images = extract_product_gallery()
+
+        products['title'] = product_title
+        products['description'] = product_desc
+        products['price'] = product_price
+        products['link'] = product_link
+        products['images'] = product_images
+
+        yield products
         pass
