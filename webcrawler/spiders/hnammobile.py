f486b19eba55cfd7ebe6dbc3b71d1da9b9052f5c
Luis Dinh <anhhai680@gmail.com>
2019-07-07 00:53:48 +0700

    Update hnammobile to scrape pagination page

--- webcrawler/spiders/hnammobile.py
+++ webcrawler/spiders/hnammobile.py
@@ -13,10 +13,32 @@ class HnammobileSpider(CrawlSpider):
     name = 'hnammobile'
     allowed_domains = ['www.hnammobile.com']
     start_urls = ['https://www.hnammobile.com/dien-thoai/']
+    rules = (
+        Rule(LxmlLinkExtractor(
+            allow=('/dien-thoai/'),
+            deny=(
+                '/dien-thoai/#',
+                '/tin-tuc/',
+                '/may-tinh-bang/',
+                '/phu-kien/',
+                '/dong-ho-thong-minh/',
+                '/kho-sim',
+                '/event/',
+                '/loai-dien-thoai/'
+            )
+        ), callback='parse_hnammobile'),
+    )
 
-    def parse(self, response):
+    def parse_hnammobile(self, response):
         for product_link in response.css('div.image>a::attr(href)'):
             yield response.follow(product_link, callback=self.parse_product_detail)
+
+        # following pagination next page to scrape
+        links = response.xpath(
+            '//li[contains(@class,"pagination-item") and (not(contains(@class,"active")))]/a/@href').getall()
+        if len(links) > 0:
+            for next_page in links:
+                yield response.follow(next_page, callback=self.parse_hnammobile)
         pass
 
     def parse_product_detail(self, response):
@@ -34,21 +56,25 @@ class HnammobileSpider(CrawlSpider):
         def extract_product_gallery(query):
             gallery = response.xpath(query).getall()
             return gallery
-        
+
         def extract_swatchcolors(query):
-            gallery = response.xpath(query).getall()
-            return gallery
+            swatchcolors = response.xpath(query).getall()
+            return swatchcolors
 
         # Validate price with pattern
         price_pattern = re.compile("([0-9](\\w+ ?)*\\W+)")
-        product_price = extract_price('//h3[contains(@class,"price")]/font/font[@class="numberprice"]/text()')
-        if re.match(price_pattern,product_price) is None:
+        product_price = extract_price(
+            '//h3[contains(@class,"price")]/font/font[@class="numberprice"]/text()')
+        if re.match(price_pattern, product_price) is None:
             return
 
         product_title = extract_with_xpath('//h2[@class="title"]/text()')
-        product_desc = extract_with_xpath('//meta[@name="description"]/@content')
-        product_swatchcolors = extract_swatchcolors('//div[@class="picker-color row"]/ul/li/div/text()')
-        product_images = extract_product_gallery('//div[@class="gallery"]/div[contains(@class,"item")]/@data-src')
+        product_desc = extract_with_xpath(
+            '//meta[@name="description"]/@content')
+        product_swatchcolors = extract_swatchcolors(
+            '//div[@class="picker-color row"]/ul/li/div//text()')
+        product_images = extract_product_gallery(
+            '//div[@class="gallery"]/div[contains(@class,"item")]/@data-src')
         #product_specifications = response.xpath('//*[@id="tskt"]/tr/*/text()').re('(\\w+[^\n]+)')
         product_specifications = []
 
