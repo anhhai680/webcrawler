e7ed29c0855450b2d41ca874ddada7fc3688269f
Luis Dinh <anhhai680@gmail.com>
2019-07-23 15:28:48 +0700

    Wasn't still scrape product detail

--- webcrawler/spiders/lazada.py
+++ webcrawler/spiders/lazada.py
@@ -18,6 +18,7 @@ class LazadaSpider(CrawlSpider):
     name = 'lazada'
     allowed_domains = ['www.lazada.vn']
     start_urls = ['https://www.lazada.vn/dien-thoai-di-dong/']
+    download_delay = 1
     rules = (
         Rule(LxmlLinkExtractor(
             allow=(
@@ -47,22 +48,31 @@ class LazadaSpider(CrawlSpider):
     )
 
     def parse_lazada(self, response):
-        #pattern = re.compile(r'window.pageData=({.*?})', re.MULTILINE)
-        #pageData = response.xpath('//script[contains(.,"window.pageData")]/text()').get()
-        pageData = re.findall(
-            "<script>window.pageData=({.+?})</script>", response.body.decode("utf-8"), re.S)
-        data = json.loads(pageData[0])
-        if data is not None:
-            for item in data["mods"]["listItems"]:
-                for product in self.parse_product_detail(item):
-                    yield product
+        logger.info('Scrape Url: %s' % response.url)
+        try:
+            pageData = re.findall(
+                "<script>window.pageData=({.+?})</script>", response.body.decode("utf-8"), re.S)
+            data = json.loads(pageData[0])
+            if data is not None:
+                for item in data["mods"]["listItems"]:
+                    for product in self.parse_product_detail(item):
+                        yield product
+
+            # Follow the next page to scrape data
+            next_page = response.xpath('//link[@rel="next"]/@href').get()
+            if next_page is not None:
+                yield response.follow(next_page, callback=self.parse_lazada)
+        except Exception as ex:
+            logger.error(
+                'Could not parse url {} with errros: {}'.format(response.url, ex))
         pass
 
     def parse_product_detail(self, item):
-        logger.info('Item: %s' % item)
+        #logger.info('Item: %s' % item)
 
         product_title = item["name"]
-        product_desc = [st.strip() for st in item["description"]]
+        #product_desc = [st.strip() for st in item["description"]]
+        product_desc = ''.join(item["description"])
         product_price = item["price"]
         product_swatchcolors = []
         product_specifications = []
@@ -70,7 +80,7 @@ class LazadaSpider(CrawlSpider):
         product_images = [st["image"] for st in item["thumbs"]]
 
         products = ProductItem()
-        products['cid'] = 1 # 1: Smartphone
+        products['cid'] = 1  # 1: Smartphone
         products['title'] = product_title
         products['description'] = product_desc
         products['price'] = product_price
