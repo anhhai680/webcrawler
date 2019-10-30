ca8492d1ffc091a52402c99875289b3f969b8fe2
anhhai680 <anhhai680@gmail.com>
2019-10-30 17:37:48 +0700

    Just updated shopee spider

--- webcrawler/spiders/shopee.py
+++ webcrawler/spiders/shopee.py
@@ -100,7 +100,13 @@ class ShopeeSpider(scrapy.Spider):
                                 product_internalmemory = attr['value']
 
                     product_brand = str(item['brand'])
-                    product_shop = None
+                    # https://shopee.vn/api/v2/shop/get?is_brief=1&shopid=23220672
+                    product_shop = 'Shopee'
+                    # shopid = item['shopid']
+                    # shoplink = 'https://shopee.vn/api/v2/shop/get?is_brief=1&shopid=%s' % shopid
+                    # if shoplink is not None:
+                    #     yield scrapy.Request(shoplink, callback=self.get_shop_name)
+
                     product_rates = item['item_rating']['rating_star']
                     product_location = str(item['shop_location'])
                     product_sku = str(item['itemid'])
@@ -136,93 +142,18 @@ class ShopeeSpider(scrapy.Spider):
             logger.error('Could not parse json data. Errors %s' % ex)
         pass
 
-    def parse_backup(self, response):
-        logger.info('Scrape Url: %s', response.url)
-        # links = response.xpath(
-        #     '//div[@class="row shopee-search-item-result__items"]/div[@class="col-xs-2-4 shopee-search-item-result__item"]/div/a/@href').getall()
-        links = response.xpath(
-            '//div[@class="row shopee-search-item-result__items"]/div/div/a/@href').getall()
-        logger.info('There is a total of ' + str(len(links)) + ' links')
-        for product_link in links:
-            try:
-                product_link = "https://shopee.vn%s" % product_link
-                yield response.follow(product_link, callback=self.parse_product_detail)
-            except:
-                pass
+    def get_shop_name(self, response):
+        # https://shopee.vn/api/v2/shop/get?is_brief=1&shopid=23220672
+        shop_name = 'Shopee'
+        try:
+            json_data = json.loads(response.body, encoding='utf-8')
+            if json_data is not None:
+                item = json_data['data']
+                if item is not None:
+                    shop_name = item['name']
+                    if shop_name is None:
+                        shop_name = item['account']['username']
+        except Exception as ex:
+            logger.error('Could not parse shop name. Errors %s' % ex)
 
-        time.sleep(1)
-
-        next_page_url = response.xpath('//link[@rel="next"]/@href').get()
-        logger.info('Next page: %s', next_page_url)
-        if next_page_url is not None:
-            match = re.match(r".*?page=(\d+)&", next_page_url)
-            next_page_number = int(match.groups()[0])
-            if next_page_number <= self.limit_pages:
-                yield response.follow(next_page_url, callback=self.parse)
-        else:
-            logger.info('Next Page was not find on page %s', response.url)
-
-    def parse_product_detail_backup(self, response):
-
-        def extract_with_xpath(query):
-            return response.xpath(query).get(default='').strip()
-
-        def extract_xpath_all(query):
-            gallery = response.xpath(query).getall()
-            return gallery
-
-        def extract_price(query):
-            price = response.xpath(query).get(default='').strip()
-            logger.info('Product Price: %s' % price)
-            if price is not None:
-                price = price.split('-')[0]
-            return price
-
-        logger.info('Parsing Url: %s', response.url)
-
-        # Validate price with pattern
-        # price_pattern = re.compile(r'(\S*[0-9](\w+?))')
-        product_price = extract_price(
-            '//div[contains(@class,"items-center")]/div[@class="_3n5NQx"]/text()')
-        # logger.info('Product Price: %s' % product_price)
-        if product_price is None or product_price == '':
-            return
-
-        product_title = extract_with_xpath('//div[@class="qaNIZv"]/text()')
-        # product_desc = extract_with_xpath(
-        #     '//div[@class="_2aZyWI"]/div[@class="_2u0jt9"]/span/text()')
-        product_desc = response.css(
-            'meta[name="description"]::attr("content")').get()
-        product_swatchcolors = extract_xpath_all(
-            '//div[@class="flex items-center crl7WW"]/button/text()')
-        product_images = response.xpath(
-            '//div[@class="_2MDwq_"]/div[@class="ZPN9uD"]/div[@class="_3ZDC1p"]/div/@style').re(r'(?:https?://).*?[^\)]+')
-
-        # product_specifications
-        product_specifications = []
-        for item in response.xpath('//div[@class="_2aZyWI"]/div[@class="kIo6pj"]'):
-            try:
-                key = item.xpath(
-                    './/label[not(contains(text(),"Danh Mục") or contains(text(),"Kho hàng") or contains(text(),"Gửi từ"))]/text()').get().strip()
-                value = item.xpath('.//a/text() | .//div/text()').get().strip()
-                product_specifications.append({key, value})
-            except:
-                pass
-
-        # body = extract_with_xpath('//div[@class="_2u0jt9"]/span/text()')
-
-        products = ProductItem(
-            cid=1,  # 1: Smartphone
-            title=product_title,
-            description=product_desc,
-            price=product_price,
-            swatchcolors=product_swatchcolors,
-            specifications=product_specifications,
-            link=response.url,
-            images=product_images,
-            shop='shopee',
-            domain='shopee.vn',
-            body=''
-        )
-
-        yield products
+        yield shop_name
