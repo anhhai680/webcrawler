17794531768b71989d2c39bff923c9c7e8d81c03
anhhai680 <anhhai680@gmail.com>
2019-11-02 11:47:13 +0700

    Successfully updated vnexpress spider

--- webcrawler/spiders/vnexpress.py
+++ webcrawler/spiders/vnexpress.py
@@ -39,6 +39,12 @@ class VnexpressSpider(CrawlSpider):
     )
     handle_httpstatus_list = [301]
 
+    custom_settings = {
+        'DOWNLOADER_MIDDLEWARES': {
+            'webcrawler.middlewares.vnexpress.VnExpressSpiderMiddleware': 543
+        },
+    }
+
     def __init__(self, limit_pages=None, *a, **kw):
         super(VnexpressSpider, self).__init__(*a, **kw)
         if limit_pages is not None:
@@ -55,7 +61,7 @@ class VnexpressSpider(CrawlSpider):
         next_page = response.xpath(
             '//ul[@class="pagination pagination-lg"]/li/a[not(contains(@class,"active"))]/@href').get()
         if next_page is not None:
-            match = re.match(r".*?page=(\d+)", next_page)
+            match = re.match(r".*/page/(\d+).html", next_page)
             if match is not None:
                 next_page_number = int(match.groups()[0])
                 if next_page_number <= self.limit_pages:
@@ -79,38 +85,87 @@ class VnexpressSpider(CrawlSpider):
         price_pattern = re.compile("([0-9](\\w+ ?)*\\W+)")
         product_price = extract_price(
             '//span[@class="price-current price_sp_detail"]/text()')
-        #logger.info('Product Price: %s' % product_price)
+        # logger.info('Product Price: %s' % product_price)
         if re.match(price_pattern, product_price) is None:
             return
+        else:
+            product_price = self.parse_money(product_price)
 
         product_title = extract_with_xpath(
-            'normalize-space(//h1[@class="product-title clearfix"]/text())')
+            '//h1[@class="product-title"]/text()')
         product_desc = extract_with_xpath(
             'normalize-space(//meta[@name="description"]/@content)')
         product_swatchcolors = extract_xpath_all(
             '//div[@class="similar-products"]/span/@rel')
         product_images = extract_xpath_all(
             '//div[@id="images_pro"]/a/@href')
-        #product_specifications = response.xpath('//div[@id="information"]/div/table[@class="table"]/tbody/tr/td//text()').getall()
+        # product_specifications = response.xpath('//div[@id="information"]/div/table[@class="table"]/tbody/tr/td//text()').getall()
+        # product_specifications = []
+        # specs = extract_xpath_all(
+        #     '//div[@class="box-body box-information"]/text()')
+        # if len(specs) > 0:
+        #     product_specifications = [sp.strip() for sp in specs]
         product_specifications = []
-        specs = extract_xpath_all(
-            '//div[@class="box-body box-information"]/text()')
-        if len(specs) > 0:
-            product_specifications = [sp.strip() for sp in specs]
+        names = extract_xpath_all(
+            '//div[@class="box-body box-information"]/table[@class="table"]/tbody/tr/td[1]//text()')
+        values = extract_xpath_all(
+            '//div[@class="box-body box-information"]/table[@class="table"]/tbody/tr/td[2]/text()')
+        if len(names) > 0 and len(values) > 0:
+            names = [' '.join(sp.split()) for sp in names]
+            names = [sp.strip() for sp in names if sp != '']
+            values = [' '.join(sp.split()) for sp in values]
+            values = [sp.strip() for sp in values if sp != '']
+            for index in range(len(names)):
+                try:
+                    if values[index] is not None and values[index] != '':
+                        product_specifications.append(
+                            [names[index], values[index]])
+                except Exception as ex:
+                    logger.error('Errors: %s' % ex)
+
+        product_oldprice = 0
+        oldprice = extract_with_xpath(
+            '//span[@class="price-old price_old_sp_detail"]/text()')
+        if oldprice is not None:
+            product_oldprice = self.parse_money(oldprice)
+
+        product_internalmemory = extract_with_xpath(
+            '//div[@class="box-body box-information"]/table[@class="table"]/tbody/tr/td[1]/label[contains(text(),"ROM")]/../td[2]/text()')
+        product_brand = extract_with_xpath(
+            '//div[@class="box-body box-information"]/table[@class="table"]/tbody/tr/td[1]/label[contains(text(),"Thương hiệu")]/../td/text()')
+        product_shop = extract_with_xpath(
+            '//div[@class="info-supplier"]/div[@class="box-name"]/p/a/text()')
+        product_rates = 0
+        product_location = extract_with_xpath(
+            '//div[@class="box-info-supplier"]/div[@class="info-address"]/p[@class="info-value"]/text()')
+        product_sku = None
+        product_instock = 1
 
         product_link = response.url
 
         products = ProductItem()
-        products['cid'] = 1  # 1: Smartphone
+        products['cid'] = 'dienthoai'  # 1: Smartphone
         products['title'] = product_title
         products['description'] = product_desc
+        products['oldprice'] = product_oldprice
         products['price'] = product_price
         products['swatchcolors'] = product_swatchcolors
+        products['internalmemory'] = product_internalmemory
         products['specifications'] = product_specifications
         products['link'] = product_link
         products['images'] = product_images
-        products["shop"] = 'vnexpress'
+        products['brand'] = product_brand
+        products["shop"] = product_shop
+        products['rates'] = product_rates
+        products['location'] = product_location
         products["domain"] = 'shop.vnexpress.net'
-        products['body'] = response.text
+        products['sku'] = product_sku
+        products['instock'] = product_instock
+        products['body'] = ''
 
         yield products
+
+    def parse_money(self, value):
+        if str(value).isdigit():
+            return value
+        return re.sub(r'[^\d]', '', str(value))
